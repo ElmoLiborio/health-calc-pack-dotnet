817c67ad340985dbcff6adc24972385a13aa72a7
Elmo Liborio <99775063+ElmoLiborio@users.noreply.github.com>
2022-11-24 20:05:42 -0300

    Delete helath-calc-pack-dotnet directory

--- helath-calc-pack-dotnet/Interfaces/IIMC.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace helath_calc_pack_dotnet.Interfaces
-{
-    public interface IIMC
-    {
-        double Calc(double peso, double altura);
-        string GetIMCCategory(double IMC);
-        bool IsValid(double peso, double altura);
-    }
-}
